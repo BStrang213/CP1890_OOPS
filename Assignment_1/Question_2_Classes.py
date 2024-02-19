e58406a0176fabb076c55d716919f38aca2bd01c
BStrang213 <144375601+BStrang213@users.noreply.github.com>
2024-02-18 21:41:01 -0330

    updates

--- Assignment_1/Question_2_Classes.py
+++ Assignment_1/Question_2_Classes.py
@@ -1,33 +1,44 @@
+# Import Random module to get Shuffle Module
 from random import shuffle
 
 
+# Create a class for Card with attributes suit and value to make the cards
 class Card:
     suit: list
     value: list
 
+    # initialize two lists to store the suits and the values to use to make the cards
     def __init__(self):
         self.suit = ['Spades', 'Clubs', 'Diamonds', 'Hearts']
         self.value = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']
 
+    # use set all values in value to v and all values in suit to s and use (v, s) to join the two to make the card and
+    # return it
     @property
     def get_card(self):
         cards = [(v, s) for v in self.value for s in self.suit]
         return cards
 
 
+# create a class for deck and create an attribute for deck_cards as list
 class Deck:
     deck_cards: list
 
+    # Get the Card class for use in the Deck class and use get_card to make the deck_cards list and set a
+    # shuffle_deck module
     def __init__(self):
         card = Card()
         self.deck_cards = card.get_card
         self.shuffle_deck()
 
+    # create a module to count all the cards on the list using the len function
     def count_cards(self):
         return len(self.deck_cards)
 
+    # set a module to shuffle the deck using random shuffle
     def shuffle_deck(self):
         shuffle(self.deck_cards)
 
+    # set a module to deal the cards to the user by popping the card out of the list to be used elsewhere
     def deal_card(self):
         return self.deck_cards.pop()
--- Assignment_1/Question_2_Program.py
+++ Assignment_1/Question_2_Program.py
@@ -1,23 +1,34 @@
+# Import from the Question_2_Classes module to use in the main program. the Deck class is the only one needed because
+# the Card class only needed through the Deck class
 from Question_2_Classes import Deck
 
 
+# set a main function.
 def main():
+    # set the Deck class as deck to be used in the function
     print("Card Dealer \n")
     deck = Deck()
-    print("I have shuffled a deck of 52 cards.\n")
-
+    # use the count_cards method to call the number of cards in the deck
+    print(f"I have shuffled a deck of {deck.count_cards()} cards.\n")
+    # get number input from the user for how many cards they want
     num = int(input("\nHow many cards would you like?: "))
+    # set an error so if the user puts in malicious inputs it will return an error
     if num <= 0 or num > deck.count_cards():
         print("Invalid Number. Try Again.")
     else:
+        # set a list for the users cards
         hand = []
+        # set a loop to give the amount of cards requested
         for i in range(num):
+            # set a variable for the cards called to be appended in the hand list
             cards = deck.deal_card()
             hand.append(cards)
         print("\nHere are your cards:")
+        # set a loop to print all the cards
         for card in hand:
             print(f"{card[0]} of {card[1]}")
 
+        # call the count_cards method to return the cards left in the deck
         print(f"\nThere are {deck.count_cards()} cards left in the deck.\n")
         print("Good Luck!")
 
