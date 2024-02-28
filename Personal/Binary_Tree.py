22940db7b85d94ee00d978daf418e03e9f0af1ed
BStrang213 <144375601+BStrang213@users.noreply.github.com>
2024-02-28 09:41:02 -0330

    Updates

---
+++ Personal/Binary_Tree.py
@@ -0,0 +1,9 @@
+# in anaconda prompt use command "conda install conda-forge::binarytree" to get the binarytree module
+
+from binarytree import build, bst
+
+nodes = [1, 11, 3, 33, 4, 44]
+
+binary_tree = bst()
+binary_tree.
+print(binary_tree)
